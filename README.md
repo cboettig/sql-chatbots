a51e8b078963fa1d22cdb28db2459eeed5176d89
Carl Boettiger <cboettig@gmail.com>
2024-04-19 16:47:28 +0000

    more models!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sql-chatbots
 
-Exploring SQL construction using LLMs
+Exploring SQL construction using free / open-weights LLMs
 
 ## Installation
 
@@ -10,23 +10,33 @@ Install python dependencies
 pip install -r requirements.txt
 ```
 
-- Install `ollama` , run `ollama serve`.
-- Download models with `ollama`: 
+- Install `ollama` , run `ollama serve`.  See <https://ollama.com/>
+- Download your choice of models with `ollama`, e.g. 
 
 ```
 ollama pull duckdb-nsql
-ollama pull llama2:70b
 ollama pull gemma
 ollama pull zephyr
 ollama pull sqlcoder
 ollama pull codegemma
 ```
 
-(Choose whatever subset you like, `duckdb-nsql` seems to works best here.)
+Larger models, requiring a GPU with greater onboard RAM:
+
+```
+ollama pull llama3:70b
+ollama pull dbrx
+ollama pull mixtral:8x22b
+ollama pull wizardlm2:8x22b
+ollama pull command-r-plus
+```
+
+(Choose whatever subset you like. `duckdb-nsql` works rather well and is small enough for many machines.)
+Comment out models you don't want in the `app.py` st.radio selector.  
+
 
 If you want to use OpenAI models, add the API key as a streamlit secret.  
 
-Comment out models you don't want in the `app.py` st.radio selector.  
 
 ## Database setup
 
--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def leaf_map(gdf):
     m.add_gdf(gdf)
     return m.to_streamlit()
 
-
 @st.cache_data
 def query_database(response):
     return con.sql(response).to_pandas().head(25)
@@ -53,7 +52,6 @@ def get_geom(tbl):
     tbl['geometry'] = tbl['geometry'].apply(wkb.loads)
     gdf = gpd.GeoDataFrame(tbl, geometry='geometry')
     return gdf
-
 
 ## Database connection
 from sqlalchemy import create_engine
@@ -66,56 +64,116 @@ import ibis
 con = ibis.connect("duckdb://pad.duckdb", read_only=True)
 con.load_extension("spatial")
 
-# alternately using duckdb directly
-#import duckdb
-#con = duckdb.connect("pad.duckdb", read_only=True)
-#con.install_extension("spatial")
-#con.load_extension("spatial")
-
 ## ChatGPT Connection
 from langchain_openai import ChatOpenAI 
-chatgpt_llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=st.secrets["OPENAI_API_KEY"])
-chatgpt4_llm = ChatOpenAI(model="gpt-4", temperature=0, api_key=st.secrets["OPENAI_API_KEY"])
 
 # Requires ollama server running locally
 from langchain_community.llms import Ollama
 ## # from langchain_community.llms import ChatOllama
 
-models = {"duckdb-nsql": Ollama(model="duckdb-nsql", temperature=0),
+models = {
+          "duckdb-nsql": Ollama(model="duckdb-nsql", temperature=0),
+          "llama3:70b": Ollama(model="llama2:70b", temperature=0),
+          "dbrx": Ollama(model="dbrx", temperature=0),
+          "command-r-plus": Ollama(model="command-r-plus", temperature=0),
+          "mixtral:8x22b":  Ollama(model="mixtral:8x22b", temperature=0),
+          "wizardlm2:8x22b":  Ollama(model="wizardlm2:8x22b", temperature=0),
           "sqlcoder": Ollama(model="sqlcoder", temperature=0),
           "zephyr": Ollama(model="zephyr", temperature=0),
           "gemma:7b": Ollama(model="gemma:7b", temperature=0),
           "codegemma": Ollama(model="codegemma", temperature=0),
-          "llama2": Ollama(model="llama2", temperature=0),
-          "chatgpt3.5": chatgpt_llm, 
-          "chatgpt4": chatgpt4_llm}
+#          "chatgpt3.5": ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=st.secrets["OPENAI_API_KEY"]), 
+#          "chatgpt4": ChatOpenAI(model="gpt-4", temperature=0, api_key=st.secrets["OPENAI_API_KEY"])
+          }
+
+map_tool = {
+            "leafmap": leaf_map, 
+            "deckgl": deck_map
+            }
+
+st.set_page_config(page_title="Protected Areas Database Chat", page_icon="🦜", layout="wide")
+st.title("Protected Areas Database Chat")
+
 with st.sidebar:
     choice = st.radio("Select an LLM:", models)
     llm = models[choice]
+    map_choice = st.radio("Select mapping tool", map_tool)
+    mapper = map_tool[map_choice]
+
+    st.markdown("*Note: switching a large model will take a while to load at first.*")
 
 ## A SQL Chain
 from langchain.chains import create_sql_query_chain
 chain = create_sql_query_chain(llm, db)
 
-
 main = st.container()
-
-## Does not preserve history
 with main:
-    if prompt := st.chat_input(key="chain"):
-        st.chat_message("user").write(prompt)
-        with st.chat_message("assistant"):
-            response = chain.invoke({"question": prompt})
-            st.write(response)
-            tbl = query_database(response)
-            if 'geometry' in tbl:
-                gdf = get_geom(tbl)
-                leaf_map(gdf)
-                n = len(gdf)
-                st.write(f"matching features: {n}")
-            st.dataframe(tbl)
+
+    '''
+    The Protected Areas Database of the United States (PAD-US) is the official national inventory of
+    America’s parks and other protected lands, and is published by the USGS Gap Analysis Project,
+    [https://doi.org/10.5066/P9Q9LQ4B.](https://doi.org/10.5066/P9Q9LQ4B).  
+
+    This interactive tool allows users to explore the dataset, as well as a range of biodiversity
+    and climate indicators associated with each protected area. These indicators are integrated into 
+    a single table format shown below.  The chatbot assistant can turn natural language queries into
+    SQL queries based on the table schema.
+
+    ##### Example Queries returning summary tables
+
+    - What is the percent area in each gap code as a fraction of the total protected area?
+    - The manager_type column indicates whether a manager is federal, state, local, private, or NGO.
+      the manager_name column indicates the responsible agency (National Park Service, Bureau of Land Management,
+      etc) in the case of federal manager types.  Which of the federal managers manage the most land in
+      gap_code 1 or 2, as a fraction of the total area?
+
+    When queries refer to specific managed areas, the chatbot can show those areas on an interactive map.
+    Do to software limitations, these maps will show no more than 25 polygons, even if more areas match the
+    requested search. The chatbot sometimes requires help identifying the right columns.  In order to create
+    a map, the SQL query must also return the geometry column.  Conisder the following examples:
+
+    ##### Example queries returning maps + tables
+
+    - Show me all the national monuments (designation_type) in Utah. Include the geometry column
+    - Show examples of Bureau of Land Management (manager_name) with the highest species richness? Include the geometry column
+    - Which site has the overall highest range-size-rarity? Include the geometry column, manager_name, and IUCN category.
+
+    '''
+
+    # Super-minimal streamlit interface
+    # Should almost surely adjust this to use chatbot state/memory
+    chatbox, database = st.columns(2)
+    with chatbox:           
+        st.markdown("## 🦜 Chatbot:")
+        st.markdown("Pick a question from above or write your own:")
+        if prompt := st.chat_input(key="chain"):
+            st.chat_message("user").write(prompt)
+            with st.chat_message("assistant"):
+                response = chain.invoke({"question": prompt})
+                st.write(response)
 
 
+    with database:
+        st.markdown("## 🗄 Database Query:")
+
+        query = st.text_input("paste SQL query here", "select manager_name from pad limit 1")
+
+        # And manually do stuff with the chatbot's SQL code
+        tbl = query_database(query)
+        if 'geometry' in tbl:
+            gdf = get_geom(tbl)
+            mapper(gdf)
+            n = len(gdf)
+            st.write(f"matching features: {n}")
+        st.dataframe(tbl)
+
+
+st.divider()
+
+with st.container():
+    st.text("Database schema (top 3 rows)")
+    tbl = tbl = query_database("select * from pad limit 3")
+    st.dataframe(tbl)
 
 # duckdb_sql fails but chatgpt3.5 succeeds with a query like:
 # use the st_area function and st_GeomFromWKB functions to compute the area of the Shape column in the fee table, and then use that to compute the total area under each GAP_Sts category
